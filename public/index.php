cd33d89c42f70c4d3bea83581fe27ba13782ef4c
yersonh <solanoalfonsoy@gmail.com>
2026-03-19 23:55:56 -0500

    first commit

--- public/index.php
+++ public/index.php
@@ -6,27 +6,27 @@ $action = $_GET['action'] ?? 'login';
 switch ($action) {
 
     case 'login':
-        require_once __DIR__ . '/../controllers/LoginController.php';
+        require_once __DIR__ . '/../Controllers/LoginController.php';
         (new LoginController())->mostrarLogin();
         break;
 
     case 'iniciarSesion':
-        require_once __DIR__ . '/../controllers/LoginController.php';
+        require_once __DIR__ . '/../Controllers/LoginController.php';
         (new LoginController())->iniciarSesion();
         break;
 
     case 'logout':
-        require_once __DIR__ . '/../controllers/LoginController.php';
+        require_once __DIR__ . '/../Controllers/LoginController.php';
         (new LoginController())->logout();
         break;
 
     case 'registro':
-        require_once __DIR__ . '/../controllers/RegistroController.php';
+        require_once __DIR__ . '/../Controllers/RegistroController.php';
         (new RegistroController())->mostrarRegistro();
         break;
 
     case 'guardarUsuario':
-        require_once __DIR__ . '/../controllers/RegistroController.php';
+        require_once __DIR__ . '/../Controllers/RegistroController.php';
         (new RegistroController())->registrar();
         break;
 
