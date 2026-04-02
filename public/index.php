2d49ce878f34e646b4ca6fb9607d8a4db613e407
Alexa123-hi! <velandiaalexandra03@gmail.com>
2026-04-02 17:24:34 -0500

    MVC

--- public/index.php
+++ public/index.php
@@ -1,22 +1,27 @@
 <?php
+session_start();
+
 require_once __DIR__ . '/../controllers/LoginController.php';
+require_once __DIR__ . '/../controllers/RegistroController.php';
 
 $action = $_GET['action'] ?? 'login';
-
-$controller = new LoginController();
 
 switch ($action) {
 
     case 'iniciarSesion':
-        $controller->iniciarSesion();
+        (new LoginController())->iniciarSesion();
         break;
 
     case 'logout':
-        $controller->logout();
+        (new LoginController())->logout();
         break;
 
     case 'registro':
         require_once __DIR__ . '/../views/Registro.php';
+        break;
+
+    case 'guardarRegistro':
+        (new RegistroController())->registrar();
         break;
 
     default:
