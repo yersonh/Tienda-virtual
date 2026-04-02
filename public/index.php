c1b73da2a31391d58e2cd4cefd43e0f55080dec9
Alexa123-hi! <velandiaalexandra03@gmail.com>
2026-04-02 17:34:57 -0500

    Mayuscula

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 
-require_once __DIR__ . '/../controllers/LoginController.php';
-require_once __DIR__ . '/../controllers/RegistroController.php';
+require_once __DIR__ . '/../Controllers/LoginController.php';
+require_once __DIR__ . '/../Controllers/RegistroController.php';
 
 $action = $_GET['action'] ?? 'login';
 
