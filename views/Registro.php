289e6315650d1fc74151f40e0a93cba8fe0e9d2b
ecoruta05 <ecoruta05@gmail.com>
2026-04-18 12:34:02 -0500

    Validaciones_Registro1

--- views/Registro.php
+++ views/Registro.php
@@ -113,16 +113,16 @@ h2 {
 }
 
 .validation-msg {
-    font-size: 10px;
-    margin-top: -6px;
-    margin-bottom: 5px;
+    font-size: 9px;
+    margin-top: -3px;
+    margin-bottom: 2px;
 
-    min-height: 10px; /* un poco más para que respire */
+    Height: 10px; /* un poco más para que respire */
     
     display: flex;              /* 🔥 esto es lo que faltaba */
     align-items: center;        /* centra vertical */
     justify-content: center;    /* centra horizontal */
-
+    padding: 0;
     line-height: 1;             /* evita desajustes */
 }
 
