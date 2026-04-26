481a55f3445116621839598253cecf3314c56c32
ecoruta05 <ecoruta05@gmail.com>
2026-04-26 16:31:42 -0500

    icono

--- views/layouts/navbar.php
+++ views/layouts/navbar.php
@@ -17,7 +17,7 @@ if (isset($_SESSION['carrito'])) {
 <head>
 <meta charset="UTF-8">
 <title>NAYLEX Store</title>
-<link rel="icon" href="public/imagenes/logosinfondo.png" type="image/png">
+<link rel="icon" href="public/imagenes/logosinfondo.ico" type="image/png">
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
